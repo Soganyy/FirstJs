5f54764c409dca9a5da441095f994d6a3b6a2bb9
Soganyy <matinmammadli0420@gmail.com>
2022-11-02 15:13:29 +0400

    deleting done

--- script.js
+++ script.js
@@ -20,16 +20,20 @@ listElements.style = "width: 80%;" +
     "flex-direction: column;";
 list.append(listElements);
 
+let i = 0;
+
 mainButton.addEventListener('click', () => {
     mainArr[mainArr.length] = inputText.value;
 
     let sec = document.createElement('div')
+    sec.className = `delLsDivBtn${i}`;
     sec.style = "style = 100%;" +
         "display: flex;" + 
         "flex-direction: row-reverse;" + 
         "margin-top: 2px";
     
     let deleteButton = document.createElement('button');
+    deleteButton.className = `deleteListElement${i}`;
     deleteButton.innerHTML = 'x';
     deleteButton.style = "border-radius: 50%;" +
         "height: 25px;" +
@@ -45,9 +49,32 @@ mainButton.addEventListener('click', () => {
     listText.style.textAlign = "left";
     sec.append(listText);
     listElements.append(sec);
+
+    // Deleting List element
+    const delLsElement = document.querySelector(`.deleteListElement${i}`);
+    const divDelete = document.querySelector(`.delLsDivBtn${i}`);
+
+    delLsElement.addEventListener('click', () => {
+        divDelete.remove();
+        
+        let numOfButton = delLsElement.className.match(/(\d+)/);
+        delete mainArr[numOfButton[0]];
+
+        // for(let m = 0; m < mainArr.length; m++){
+        //     const index = array.indexOf(undefined);
+        //     mainArr.splice(index, 1);
+        // }
+        // i--;
+
+        // console.log(mainArr);
+    })
+
+    i++;
 });
 
+// Delete button in input
 delBtnFirst.addEventListener('click', () => {
     inputText.value = '';
 })
 
+// Sorting
